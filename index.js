8166f2fa58c5395aa7a43ce0ad218c7088a99320
乐莉 <1328880321@qq.com>
2019-11-09 21:28:22 +0800

    修改

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 const express = require('express')
 const app = express()
-const router = express().router()
-const path = require('path')
-const fs = require('fs')
+const fileRouter = require('./files/load-file')
+// const path = require('path')
+// const fs = require('fs')
 
-router.use(function(req, res, next) {
-    // .. some logic here .. like any other middleware
-    next();
-  });
-  
-  // will handle any request that ends in /events
-  // depends on where the router is "use()'d"
-router.get('/events', function(req, res, next) {
-// ..
-});
 
-app.use('/calendar', router);
+app.use('/file', fileRouter);
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
