cbd4ca75c166adc087efe50ef052f891687da917
乐莉 <1328880321@qq.com>
2019-11-10 18:11:27 +0800

    添加文件上传测试页面

--- files/load-file.js
+++ files/load-file.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+
 
 router.get('/', function(req, res, next) {
-    res.send('getfiles')
+
 });
+router.post('/post', jsonParser, function(req, res, next) {
+  console.log("post:", req)
+  res.header("Access-Control-Allow-Origin", "*")
+  res.json({
+    name: "收到了",
+  })
+});
+
 module.exports = router
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
 const fileRouter = require('./files/load-file')
 // const path = require('path')
 // const fs = require('fs')
-
+const port = 3000
 
 app.use('/file', fileRouter);
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
